752e4a56402eda9e284c5183e10af6eed23436df
haruka.murakami <haruka.murakami@optim.co.jp>
2025-10-03 00:31:10 +0900

    cotroller: index, new, create, live_event_paramsのアクションをlive_eventsに追加

--- app/controllers/live_events_controller.rb
+++ app/controllers/live_events_controller.rb
@@ -1,11 +1,26 @@
 class LiveEventsController < ApplicationController
   def index
+    @live_events = LiveEvent.all
   end
 
   def show
   end
 
   def new
+    @live_event = LiveEvent.new
+  end
+
+  def create
+    @live_event = LiveEvent.new(live_event_params)
+    if @live_event.save
+      redirect_to live_events_path, notice: "ライブを登録しました！"
+    else
+      render :new
+    end
+  end
+
+  def live_event_params
+    params.require(:live_event).permit(:title, :date, :veune, :artist)
   end
 
   def edit
