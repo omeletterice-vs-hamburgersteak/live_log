134958639ae95aac55a8f015974aaf5401bb2b20
haruka.murakami <haruka.murakami@optim.co.jp>
2025-10-04 19:47:38 +0900

    add: edit,update,show,destroyを追加

--- app/controllers/live_events_controller.rb
+++ app/controllers/live_events_controller.rb
@@ -24,5 +24,25 @@ class LiveEventsController < ApplicationController
   end
 
   def edit
+    @live_event = LiveEvent.find(params[:id])
+  end
+
+  def update
+    @live_event = LiveEvent.find(params[:id])
+    if @live_event.update(live_event_params)
+      redirect_to live_events_path, notice: "更新しました！"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @live_event = LiveEvent.find(params[:id])
+  end
+
+  def destroy
+    @live_event = LiveEvent.find(params[:id])
+    @live_event.destroy
+    redirect_to live_events_path, notice: "削除しました！"
   end
 end
