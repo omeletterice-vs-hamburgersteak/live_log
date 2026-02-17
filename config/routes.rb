e17b49491a8ce16e9b38641af43032d8b9be5a30
haruka.murakami <haruka.murakami@optim.co.jp>
2026-02-17 23:48:14 +0900

    fix: rootの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  Rails.application.routes.draw do
-    resources :live_events
-  end
+  resources :live_events
+  root "live_events#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
