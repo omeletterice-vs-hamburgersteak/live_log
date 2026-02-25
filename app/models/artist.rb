117dcb2e11c80f42d251a1993fd79258c05e16f3
haruka.murakami <haruka.murakami@optim.co.jp>
2026-02-26 01:03:40 +0900

    fix: favoriteモデルとartistの紐づけ

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,9 @@
 class Artist < ApplicationRecord
   has_many :live_event_artists
   has_many :live_events, through: :live_event_artists
+
+  has_many :favorite_artists
+  has_many :favorites, through: :favorite_artists
+
   validates :name, presence: true
 end
