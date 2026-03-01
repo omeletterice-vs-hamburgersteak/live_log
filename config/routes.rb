fdb1ec7e062837ea38fd3ba3b4b7957ed699d8e2
haruka.murakami <haruka.murakami@optim.co.jp>
2026-03-01 15:21:46 +0900

    fix: rootの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get "home/index"
   resources :favorites
   resources :live_events
-  resources :artists, only: [:index, :show, :new, :create]
-  root "live_events#index"
+  resources :artists
+  root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
