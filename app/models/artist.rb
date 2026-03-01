5b55932833982a3992e50424464fa81c634a4366
haruka.murakami <haruka.murakami@optim.co.jp>
2026-03-01 16:04:56 +0900

    test: artistモデルによるテスト落ちの修正

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,7 +2,7 @@ class Artist < ApplicationRecord
   has_many :live_event_artists, dependent: :destroy
   has_many :live_events, through: :live_event_artists
 
-  has_many :favorite_artists
+  has_many :favorite_artists, dependent: :destroy
   has_many :favorites, through: :favorite_artists
 
   validates :name, presence: true
